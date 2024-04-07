704b2b7659dbcca311dc352240f2eb2330c93901
Ugo Obiako <obiakou@yahoo.com>
2024-04-07 12:45:28 -0700

    Added a file

---
+++ test-code.py
@@ -0,0 +1,12 @@
+msg = "hello world"
+print(msg)
+print("the system is set up as shown")
+
+a=20
+if a>2:
+    print("a is greater than 2")
+else:
+    print("a is not greater than 2") # Indentation is very important in Python
+
+    # Note that this is a comment 
+    # The system configuration parameters
